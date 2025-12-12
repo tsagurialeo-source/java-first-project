eb01e6f33ff6146cb40b9354715bca7c6146e3ad
Цагурия Лео <tsagurialeo@gmail.com>
2025-12-12 09:39:00 +0300

    изменил код для поиска максимума

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,10 @@
-package org.example;
-
-public class Main {
+public class FindMaxOfFour {
     public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
-
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
-
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
-
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        int a = 15;
+        int b = 42;
+        int c = 7;
+        int d = 89;
+        int max = Math.max(Math.max(a, b), Math.max(c, d));
+        System.out.println("Максимальное число : " + max);
     }
 }
